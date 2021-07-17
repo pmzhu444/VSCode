eeee551be5f303ebb395566f9390e0e8d2e90b93
pmzhu_Lab <pmzhu444@163.com>
2021-07-17 15:00:10 +0800

    leetcode

---
+++ leetcode/80.删除有序数组中的重复项-ii.java
@@ -0,0 +1,26 @@
+/*
+ * @lc app=leetcode.cn id=80 lang=java
+ *
+ * [80] 删除有序数组中的重复项 II
+ */
+
+// @lc code=start
+class Solution {
+    public int removeDuplicates(int[] nums) {
+        int n = nums.length;
+        if (n <= 2) {
+            return n;
+        }
+        int slow = 2, fast = 2;
+        while (fast < n) {
+            if (nums[slow - 2] != nums[fast]) {
+                nums[slow] = nums[fast];
+                ++slow;
+            }
+            ++fast;
+        }
+        return slow;
+    }
+}
+// @lc code=end
+
