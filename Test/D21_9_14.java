79e6dc9ace0193cdd9ab16ad7040e75585ff0436
pmzhu_Lab <pmzhu444@163.com>
2021-09-14 15:31:16 +0800

    test

---
+++ Test/D21_9_14.java
@@ -0,0 +1,9 @@
+import jdk.javadoc.internal.doclets.formats.html.SourceToHTMLConverter;
+
+public class D21_9_14 {
+    public static void main(String[] args) {
+        String s = "a";
+        String t = "b";
+        System.out.println(s.compareTo(t));
+    }
+}
